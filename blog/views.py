33fdab6432e173b5424db2a3442502316709bc65
minsoo <rms5254@naver.com>
2023-11-12 06:23:27 +0900

    11.12 민수 기본틀잡기

---
+++ blog/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
+
+
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
+
+class PostDetail(DetailView):
+    model = Post
+
+# Create your views here.
+# Class PostDetail(DetailView):
+#     model = Post
+#    def landing(request):
+#    return render(
+#        request,
+#        'single_pages/landing.html',
+#    )
+
+def single_post_page(request, pk):
+
+    post = Post.objects.get(pk=pk)
+
+    return render(
+        request,
+        'blog/post_detail.html',
+        {
+            'post':post,
+        }
+    )
